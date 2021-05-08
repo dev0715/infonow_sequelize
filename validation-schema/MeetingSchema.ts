3e125ecc8589e6f7d6fdd517ac19693f015766ed
dev0715 <toptal126@outlook.com>
2021-05-08 13:03:47 +0500

    a function added

--- validation-schema/MeetingSchema.ts
+++ validation-schema/MeetingSchema.ts
@@ -5,29 +5,29 @@ export const NewMeetingSchema = Joi.object({
 	guest: Joi.string()
 		.uuid()
 		.required()
-		.error(new ValidationError("participant is required")),
+		.error(new ValidationError("%s is required", "participant")),
 
 	scheduledAt: Joi.date()
 		.required()
-		.error(new ValidationError("date is required")),
+		.error(new ValidationError("%s is required", "date")),
 
 	createdBy: Joi.string()
 		.uuid()
 		.required()
-		.error(new ValidationError("userId is required")),
+		.error(new ValidationError("%s is required", "userId")),
 });
 
 export const AcceptOrRejectMeetingSchema = Joi.object({
 	meetingId: Joi.string()
 		.uuid()
 		.required()
-		.error(new ValidationError("meetingId is required")),
-	
+		.error(new ValidationError("%s is required", "meetingId")),
+
 	status: Joi.string()
 		.valid(...["accepted", "rejected"])
 		.required()
-		.error(new ValidationError("status is required")),
-	
+		.error(new ValidationError("%s is required", "status")),
+
 	message: Joi.string().min(0).max(200),
 });
 
@@ -35,20 +35,20 @@ export const CancelOrRescheduleMeetingSchema = Joi.object({
 	meetingId: Joi.string()
 		.uuid()
 		.required()
-		.error(new ValidationError("meetingId is required")),
-	
+		.error(new ValidationError("%s is required", "meetingId")),
+
 	status: Joi.string()
 		.valid(...["cancelled", "rescheduled"])
 		.required()
-		.error(new ValidationError("status is required")),
-	
+		.error(new ValidationError("%s is required", "status")),
+
 	scheduledAt: Joi.date()
 		.required()
-		.error(new ValidationError("date is required")),
-	
+		.error(new ValidationError("%s is required", "date")),
+
 	message: Joi.string()
 		.min(0)
 		.max(200)
 		.required()
-		.error(new ValidationError("message is required")),
+		.error(new ValidationError("%s is required", "message")),
 });
