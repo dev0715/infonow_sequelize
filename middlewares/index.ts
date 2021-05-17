6ba8faf7ce85506ab5d9e222921cb7d20b4731be
dev0715 <toptal126@outlook.com>
2021-05-17 14:22:02 +0500

    feat: Error Handling and typesense

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,8 +1,10 @@
 import { User } from '../models/User';
 
-declare module 'express' {
-	interface Request {
-        CurrentUser?: User,
-        __: i18nAPI
-	}
+declare global{
+    namespace Express {
+        interface Request {
+			CurrentUser?: User,
+			__?: i18nAPI
+        }
+    }
 }
