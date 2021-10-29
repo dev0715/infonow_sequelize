0550fcc99b0921b7d6bbf3d5e7e19577720f166b
dev0715 <toptal126@outlook.com>
2021-10-29 20:26:13 +0500

    user auth and add schema updated , recaptcha added

--- validation-schema/UserAuthSchema.ts
+++ validation-schema/UserAuthSchema.ts
@@ -1,17 +1,16 @@
-import Joi from 'joi';
-import { ValidationError } from '../utils/errors';
+import Joi from "joi";
+import { ValidationError } from "../utils/errors";
 
 export const UserAuthSchema = Joi.object({
+  email: Joi.string().email().required(),
 
-    email: Joi.string()
-        .email()
-        .required(),
+  password: Joi.string()
+    .min(8)
+    .max(30)
+    .required()
+    .error(new ValidationError("Password must be between 8-30 characters")),
 
-    password: Joi.string()
-        .min(4)
-        .max(30)
-        .required()
-        .error(new ValidationError("Password must be between 8-30 characters")),
-
-
-})
+  reCaptchaToken: Joi.string()
+    .optional()
+    .error(new ValidationError("Enable ReCaptcha")),
+});
